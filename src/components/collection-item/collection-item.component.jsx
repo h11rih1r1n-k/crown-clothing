dd5f44bbac9069444daa2a26da55ff09cba31ee8
h11rih1r1n-k <for.code.platform@gmail.com>
2021-12-17 16:48:07 +0530

    adding header component and adding links to home, shop page, contact

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -12,7 +12,7 @@ const CollectionItem = ({ imageUrl, name, price }) => {
       ></div>
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">{`$${price}`}</span>
       </div>
     </div>
   );
