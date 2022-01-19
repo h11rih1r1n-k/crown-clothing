952753ce71d60dd7ea994252a0bcfa7dd35f9847
h11rih1r1n-k <for.code.platform@gmail.com>
2022-01-19 17:42:14 +0530

    added director and shop reducer moved static data into them

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import "./shop.styles.scss";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ selectShopCollections }) => (
+  <div>
+    {selectShopCollections.map(({ id, ...otherCollection }) => (
+      <CollectionPreview key={id} {...otherCollection} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  selectShopCollections,
+});
 
-  render() {
-    return (
-      <div>
-        {this.state.collections.map(({ id, ...otherCollection }) => (
-          <CollectionPreview key={id} {...otherCollection} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
